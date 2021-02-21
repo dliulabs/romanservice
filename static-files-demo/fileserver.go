3261b2139a3791ffb611b0f817bcf1e8d165a518
david.s.liu@gmail.com <david.s.liu@gmail.com>
2021-02-21 12:49:52 -0600

    add static file server

---
+++ static-files-demo/fileserver.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net/http"
+)
+
+func main() {
+	router := httprouter.New()
+	// Mapping to methods is possible with HttpRouter
+	router.ServeFiles("/static/*filepath",
+		http.Dir("./static"))
+	log.Printf("http://localhost:8000/static/latin.txt")
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
